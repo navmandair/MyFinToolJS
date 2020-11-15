11edc2b67fe4f89b59017f497df5804142fb9bcd
Navpreet Singh <nav.mandair7@gmail.com>
2020-11-15 03:37:19 +0000

    5th

--- index.js
+++ index.js
@@ -37,19 +37,17 @@ function sip_performance(data, amount_investing) {
   number_of_shares = 0;
   data.forEach(el =>
   { 
-    console.log(el.date, el.adjClose);
     amount_invested = amount_invested + amount_investing;
     number_of_shares = number_of_shares + (amount_investing/el.adjClose);
   });
   oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  first_date = data[0].date;
-  last_date = data[data.length-1].date;
-  days_invested = Math.round(Math.abs((first_date - last_date) / oneDay));
+  last_date = data[0].date;
+  first_date = data[data.length-1].date;
+  days_invested = Math.round(Math.abs((last_date - first_date) / oneDay));
   years_invested = days_invested/365;
   last_price = data[0].adjClose;
   current_value = number_of_shares * last_price;
   total_gain = (current_value - amount_invested)/amount_invested * 100;
   performance_pa = total_gain/years_invested;
-    console.log(total_gain, years_invested);
-  return {"performance_pa":performance_pa}
+  return {"first_date":first_date, "last_date":last_date, "years_invested": years_invested,"performance_pa":performance_pa}
 }
