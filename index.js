fd39633b994b004fb986c960e9551e965b0f2e6d
Navpreet Singh <nav.mandair7@gmail.com>
2020-11-16 08:38:36 +0000

    8th

--- index.js
+++ index.js
@@ -1,53 +1,23 @@
-const yahooFinance = require('yahoo-finance')
 const express = require('express')
-const app = express()
-const port = 3000
+const path = require('path');
+const app = express();
+const indexRouter = require('./routes/index');
 
-app.use(express.urlencoded({
-  extended: true
-}))
+app.use('/', indexRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.engine('pug', require('pug').__express)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/Views/" + "index.html");
-})
 
-app.post('/result', (req, res) => {
-
-  yahooFinance.historical({
-    symbol: req.body.ticker_search,
-    from: req.body.start_date,
-    to: req.body.end_date,
-    period: req.body.period
-  }, function(err, quotes) {
-    if(quotes.length != 0)
-      {res.send(sip_performance(quotes, 50));}
-    else
-      {res.send({})}
-  });
-  
-})
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
-function sip_performance(data, amount_investing) {
-  amount_invested = 0;
-  number_of_shares = 0;
-  data.forEach(el =>
-  { 
-    amount_invested = amount_invested + amount_investing;
-    number_of_shares = number_of_shares + (amount_investing/el.adjClose);
-  });
-  oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  last_date = data[0].date;
-  first_date = data[data.length-1].date;
-  days_invested = Math.round(Math.abs((last_date - first_date) / oneDay));
-  years_invested = days_invested/365;
-  last_price = data[0].adjClose;
-  current_value = number_of_shares * last_price;
-  total_gain = (current_value - amount_invested)/amount_invested * 100;
-  performance_pa = total_gain/years_invested;
-  return {"first_date":first_date, "last_date":last_date, "years_invested": years_invested,"performance_pa":performance_pa}
-}
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const yahooFinance = require('yahoo-finance');
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/result', (req, res) => {
+  console.log(req);
+  yahooFinance.historical({
+    symbol: req.query.ticker_search,
+    from: req.query.start_date,
+    to: req.query.end_date,
+    period: req.query.period
+  }, function(err, quotes) {
+    if(quotes.length != 0)
+      {res.send(sip_performance(quotes, 50));}
+    else
+      {res.send({})}
+  });
+  
+})
+
+
+function sip_performance(data, amount_investing) {
+  amount_invested = 0;
+  number_of_shares = 0;
+  data.forEach(el =>
+  { 
+    amount_invested = amount_invested + amount_investing;
+    number_of_shares = number_of_shares + (amount_investing/el.adjClose);
+  });
+  oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+  last_date = data[0].date;
+  first_date = data[data.length-1].date;
+  days_invested = Math.round(Math.abs((last_date - first_date) / oneDay));
+  years_invested = days_invested/365;
+  last_price = data[0].adjClose;
+  current_value = number_of_shares * last_price;
+  total_gain = (current_value - amount_invested)/amount_invested * 100;
+  performance_pa = total_gain/years_invested;
+  return {"first_date":first_date, "last_date":last_date, "years_invested": years_invested,"performance_pa":performance_pa}
+}
+
+
+module.exports = router;
