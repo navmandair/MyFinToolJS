435199b39be51035022d5b2cfdcc97e928243b0a
Navpreet Singh <nav.mandair7@gmail.com>
2020-11-18 09:20:46 +0000

    12th

--- routes/index.js
+++ routes/index.js
@@ -12,46 +12,63 @@ router.get('/result', (req, res) => {
   var from_date = req.query.start_date;
   var to_date = req.query.end_date
   var period = req.query.period;
-  var rows = [];
+  var period = req.query.period;
+  var amount = parseInt(req.query.amount);
   yahooFinance.historical({
     symbol: ticker,
     from: from_date,
     to: to_date,
-    period: period
+    period: "d"
   }, 
   function(err, quotes) 
   {
     if (quotes.length != 0) 
-    { rows = sip_performance(ticker, period, quotes, 50);
-     res.render('performance', { "rows": rows }); }
+    {
+      res.render('performance', sip_performance(ticker, from_date, period, quotes.reverse(), amount)) 
+    }
+    else
+    {
+      res.render('performance', {"rows": [], "data_rows": []})
+    }
   });
 })
 
 
-function sip_performance(ticker, period, data, amount_investing) {
+function sip_performance(ticker, from_date, period, data, amount_investing) {
   amount_invested = 0;
   number_of_shares = 0;
+  day_of_investing = new Date(from_date).getDay();
+
+  new_data = [];
   data.forEach(el => {
+    if(period == "d" || (period == "w" && new Date(el.date).getDay() == day_of_investing))
+    {
+    new_data.push(el);
     amount_invested = amount_invested + amount_investing;
     number_of_shares = number_of_shares + (amount_investing / el.adjClose);
+    }
   });
+  
   oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  last_date = data[0].date;
-  first_date = data[data.length - 1].date;
+  first_date = new_data[0].date;
+  last_date = new_data[new_data.length - 1].date;
+  last_price = new_data[new_data.length - 1].adjClose;
   days_invested = Math.round(Math.abs((last_date - first_date) / oneDay));
   years_invested = days_invested / 365;
-  last_price = data[0].adjClose;
   current_value = number_of_shares * last_price;
   total_gain = (current_value - amount_invested) / amount_invested * 100;
   performance_pa = total_gain / years_invested;
-  return [{
+  return {"rows": [{
     "ticker": ticker,
-    "start_date": new Date(first_date).toLocaleString('en-us'),
-    "end_date": new Date(last_date).toLocaleString('en-us'), 
+    "start_date": new Date(first_date).toDateString(),
+    "end_date": new Date(last_date).toDateString(), 
     "period": period, 
+    "amount_invested": amount_invested,
+    "current_value": current_value,
     "total_gain": total_gain,
     "performance_pa": performance_pa
-  }]
+  }],
+  "data_rows": new_data}
 }
 
 
