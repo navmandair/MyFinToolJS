08616c23645cd90ec2feffa1dd7135abd3ff911c
Navpreet Singh <nav.mandair7@gmail.com>
2020-11-19 10:16:50 +0000

    15th

--- public/custom.js
+++ public/custom.js
@@ -1,19 +1,29 @@
-$( function() {
-  $( "#ticker" ).autocomplete({
-      source: function( request, response ) {
-        $.ajax( {
-          url: "/ticker/search",
-          dataType: "json",
-          data: {
-            search_key: request.term
-          },
-          success: function( data ) {
-            response( data );
-          }
-        } );
-      },
-      minLength: 1,
-      select: function( event, ui ) {
-      }
-    } );
-  } );
+$(function() {
+  $("#ticker").autocomplete({
+    source: function(request, response) {
+      $.ajax({
+        url: "/ticker/search",
+        dataType: "json",
+        data: {
+          search_key: request.term
+        },
+        success: function(data) {
+          response(data);
+        }
+      });
+    },
+    minLength: 1,
+    select: function(event, ui) {
+    }
+  });
+});
+
+
+$(document).ready(function() {
+  var now = new Date();
+  var day = ("0" + now.getDate()).slice(-2);
+  var month = ("0" + (now.getMonth() + 1)).slice(-2);
+  var today = now.getFullYear() + "-" + (month) + "-" + (day);
+  $('#end_date').val(today);
+
+});
